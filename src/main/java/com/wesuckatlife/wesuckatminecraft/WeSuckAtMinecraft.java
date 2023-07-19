11fe3921792bbe270cd44728662d2828b2fd58c1
WSAL Evan <evan@millers6.com>
2023-07-18 22:38:31 -0400

    Added Ruby

--- src/main/java/com/wesuckatlife/wesuckatminecraft/WeSuckAtMinecraft.java
+++ src/main/java/com/wesuckatlife/wesuckatminecraft/WeSuckAtMinecraft.java
@@ -1,6 +1,7 @@
 package com.wesuckatlife.wesuckatminecraft;
 
 import com.mojang.logging.LogUtils;
+import com.wesuckatlife.wesuckatminecraft.item.ModItems;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -21,6 +22,8 @@ public class WeSuckAtMinecraft
     public WeSuckAtMinecraft()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
---
+++ src/main/java/com/wesuckatlife/wesuckatminecraft/item/ModItems.java
@@ -0,0 +1,23 @@
+package com.wesuckatlife.wesuckatminecraft.item;
+
+import com.wesuckatlife.wesuckatminecraft.WeSuckAtMinecraft;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems
+{
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, WeSuckAtMinecraft.MODID);
+
+    public static final RegistryObject<Item> RUBY = ITEMS.register("ruby",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS)));
+
+    public static void register(IEventBus eventBus)
+    {
+        ITEMS.register(eventBus);
+    }
+}
