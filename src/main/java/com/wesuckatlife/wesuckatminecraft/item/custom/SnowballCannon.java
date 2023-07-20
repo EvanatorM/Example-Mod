44874e7b1175efb5a960bf1395d50bdbb7b97218
WSAL Evan <evan@millers6.com>
2023-07-20 19:57:17 -0400

    Improved Snowball Cannon

--- src/main/java/com/wesuckatlife/wesuckatminecraft/item/custom/SnowballCannon.java
+++ src/main/java/com/wesuckatlife/wesuckatminecraft/item/custom/SnowballCannon.java
@@ -1,7 +1,9 @@
 package com.wesuckatlife.wesuckatminecraft.item.custom;
 
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.Snowball;
 import net.minecraft.world.item.Item;
@@ -17,17 +19,29 @@ public class SnowballCannon extends Item
         super(properties);
     }
 
+
+
     @Override
     public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand)
     {
         if (!level.isClientSide() && hand == InteractionHand.MAIN_HAND)
-        {
-            Snowball snowball = new Snowball(level, player);
-            snowball.setItem(new ItemStack(Items.SNOWBALL));
-            snowball.shootFromRotation(player, player.getXRot(), player.getYRot(), 0.0f, 2.5f, 1.0f);
-            level.addFreshEntity(snowball);
-        }
+            player.startUsingItem(hand);
 
         return super.use(level, player, hand);
+    }
+    @Override
+    public void onUsingTick(ItemStack stack, LivingEntity player, int count)
+    {
+        Snowball snowball = new Snowball(player.getLevel(), player);
+        snowball.setItem(new ItemStack(Items.SNOWBALL));
+        snowball.shootFromRotation(player, player.getXRot(), player.getYRot(), 0.0f, 2.5f, 1.0f);
+        player.getLevel().addFreshEntity(snowball);
+
+        super.onUsingTick(stack, player, count);
+    }
+
+    @Override
+    public int getUseDuration(ItemStack pStack) {
+        return 100;
     }
 }
